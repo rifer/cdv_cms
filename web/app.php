9ba9f7d1f2d2bd3c70c1192e4e9b78965c4a9037
Rodrigo Iglesias <you@example.com>
2015-10-01 13:32:44 +0200

    First clean installation

---
+++ web/app.php
@@ -0,0 +1,43 @@
+<?php
+
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+if (extension_loaded('apc') && ini_get('apc.enabled')) {
+    // Enable APC for autoloading to improve performance.
+    // You should change the ApcClassLoader first argument to a unique prefix
+    // in order to prevent cache key conflicts with other applications
+    // also using APC.
+    $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+    $loader->unregister();
+    $apcLoader->register(true);
+}
+
+require_once __DIR__.'/../app/AppKernel.php';
+
+if (getenv('APP_ENV') === 'dev') {
+    umask(0000);
+    Debug::enable();
+    $kernel = new AppKernel('dev', true);
+} else {
+    $kernel = new AppKernel('prod', false);
+}
+
+$kernel->loadClassCache();
+
+if (getenv('APP_ENV') !== 'dev') {
+    if (!isset($_SERVER['HTTP_SURROGATE_CAPABILITY']) || false === strpos($_SERVER['HTTP_SURROGATE_CAPABILITY'], 'ESI/1.0')) {
+        require_once __DIR__.'/../app/AppCache.php';
+        $kernel = new AppCache($kernel);
+    }
+}
+
+Request::enableHttpMethodParameterOverride();
+Request::setTrustedProxies(array('127.0.0.1'));
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
